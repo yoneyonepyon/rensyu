a1d4a28a653544d147b25bd8ad5c975caae62ce8
ぴょんぴょん <46988423+yoneyonepyon@users.noreply.github.com>
2019-01-29 07:41:53 +0900

    ３→４

--- Selection.java
+++ Selection.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 /*
- *  3つの数値をランダムに出して、それを選択ソートする
+ *  4つの数値をランダムに出して、それを選択ソートする
  *  (記憶を頼りに)
  */
 public class Selection {
@@ -12,14 +12,15 @@ public class Selection {
 		int max1 = r.nextInt(100);
 		int max2 = r.nextInt(100);
 		int max3 = r.nextInt(100);
+		int max4 = r.nextInt(100);
 		int temp;
 		
-		System.out.println("初期値: " + max1 + ", " + max2 + ", " + max3);
+		System.out.println("初期値: " + max1 + ", " + max2 + ", " + max3 + ", " + max4);
 		
-		if (max2 > max1) {
-			temp = max1;
-			max1 = max2;
-			max2 = temp;
+		if (max4 > max3) {
+			temp = max3;
+			max3 = max4;
+			max4 = temp;
 		}
 
 		if (max3 > max2) {
@@ -34,8 +35,25 @@ public class Selection {
 			max2 = temp;
 		}
 
+		if (max4 > max3) {
+			temp = max3;
+			max3 = max4;
+			max4 = temp;
+		}
+
+		if (max3 > max2) {
+			temp = max2;
+			max2 = max3;
+			max3 = temp;
+		}
 		
-		System.out.println("変更後: " + max1 + ", " + max2 + ", " + max3);		
+		if (max4 > max3) {
+			temp = max3;
+			max3 = max4;
+			max4 = temp;
+		}
+
+		System.out.println("変更後: " + max1 + ", " + max2 + ", " + max3 + ", " + max4);
 		
 	}
 
