be7eb125aa8cba7cdf75a1d0031bdd8e69cc36cb
ぴょんぴょん <46988423+yoneyonepyon@users.noreply.github.com>
2019-01-30 07:50:41 +0900

    シーザー暗号

---
+++ caesar.py
@@ -0,0 +1,17 @@
+# https://ja.wikipedia.org/wiki/シーザー暗号
+
+
+def cipher(sentense, key=3):
+    m = ord('A')
+    n = ord('Z')
+    for c in sentense:
+        d = ord(c)
+        if m <= d and d <= n:
+            c = chr(((d - key) - m) % 26 + m)
+        print(c, end='')
+    print()
+
+
+if __name__ == "__main__":
+    plain = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG'
+    cipher(plain)  # QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD
