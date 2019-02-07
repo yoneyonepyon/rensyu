5545f00b0faa0f32522e7bc22b4d39fb5ba63265
ぴょんぴょん <46988423+yoneyonepyon@users.noreply.github.com>
2019-02-07 12:45:29 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        char[] string = { 'H', 'e', 'l', 'l', 'o', '!', '\n' };
+        for (char c : string) {
+            System.out.print(c);
+        }
     }
 }
